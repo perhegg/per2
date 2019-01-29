54e3a841f8141aa966ad853a525a4f7424a18b64
olivia <olivia@evolve.se>
2019-01-29 14:49:35 +0100

    modify features and create helper method

---
+++ features/support/world.js
@@ -0,0 +1,20 @@
+const { setWorldConstructor } = require('cucumber')
+const { expect } = require('chai')
+const puppeteer = require('puppeteer')
+
+
+const HOME_PAGE = 'http://localhost:3000'
+
+class AddressBookWorld {
+    constructor() {}
+        async openHomePage() {
+            this.browser = await puppeteer.launch({headless: false, slowmo: 100})
+            this.page = await this.browser.newPage()
+            await this.page.goto(HOME_PAGE)
+        }
+        async closeHomePage() {
+            await this.browser.close()
+          }
+}
+
+setWorldConstructor(AddressBookWorld)
